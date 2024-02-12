f2ddd910e94529b7bdbcd127222c420dffcdb4da
nrg-biker <156887367+nrg-biker@users.noreply.github.com>
2024-02-12 17:58:02 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,17 @@
+function generateRandomData() {
+
+    }
+    const data = [];
+}
+function main() {
+    for (let i = 0; i < 10; i++) {
+    data.forEach(item => {
+}
+
+    const data = generateRandomData();
+    return data;
+main();
+        console.log("Random Number: " + item);
+
+    });
+        data.push(Math.floor(Math.random() * 100) + 1);
