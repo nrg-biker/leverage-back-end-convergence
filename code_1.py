f3bc627fd4ee0d35449b6d2cd9ce9f397a4cae52
nrg-biker <156887367+nrg-biker@users.noreply.github.com>
2024-02-23 18:12:46 +0100

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,27 +1,14 @@
-
-def generate_random_data():
-
-def main():
-
-        print(f"Random Number: {item}")
-    data = [random.randint(1, 100) for _ in range(10)]
-    data = generate_random_data()
     main()
-import random
-    for item in data:
-if __name__ == "__main__":
-
-    return data    main()
-
     return data
-def generate_random_data():
-if __name__ == "__main__":
-
-
-        print(f"Random Number: {item}")
-    data = generate_random_data()
-import random
-
     data = [random.randint(1, 100) for _ in range(10)]
     for item in data:
+import random
+
 def main():
+if __name__ == "__main__":
+
+        print(f"Random Number: {item}")
+
+def generate_random_data():
+
+    data = generate_random_data()
